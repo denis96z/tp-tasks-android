a28a2ef3a15018c050409380a2981f99a5a4971d
denis96z <zinoviev.denis@yandex.ru>
2019-03-15 22:56:31 +0300

    AndroidX

--- task01/app/src/main/java/ru/mail/park/android/zinovyev/task01/MainActivity.java
+++ task01/app/src/main/java/ru/mail/park/android/zinovyev/task01/MainActivity.java
@@ -1,10 +1,9 @@
 package ru.mail.park.android.zinovyev.task01;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
