66a357b2cd27bc3fab60aa89c913cf3789aa19bd
denis96z <zinoviev.denis@yandex.ru>
2019-03-22 18:52:09 +0300

    button to change activity added

--- task01/app/src/main/java/ru/mail/park/android/zinovyev/task01/MainActivity.java
+++ task01/app/src/main/java/ru/mail/park/android/zinovyev/task01/MainActivity.java
@@ -1,12 +1,18 @@
 package ru.mail.park.android.zinovyev.task01;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view) {
+        startActivity(new Intent(this, TableActivity.class));
     }
 }
