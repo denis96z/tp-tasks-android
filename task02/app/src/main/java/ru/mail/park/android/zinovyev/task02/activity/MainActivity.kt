681854330a23b4906fee6724b5399a54ad4eaa58
denis96z <zinoviev.denis@yandex.ru>
2019-03-27 00:03:38 +0300

    task02: initial commit

---
+++ task02/app/src/main/java/ru/mail/park/android/zinovyev/task02/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.mail.park.android.zinovyev.task02.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.mail.park.android.zinovyev.task02.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
